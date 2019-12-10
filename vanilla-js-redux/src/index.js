de936a084cd323786326b07c01989ba7835e6684
james.kim <james.kim@yht.co.kr>
2019-12-10 15:30:15 +0900

    바닐라 js 리덕스

---
+++ vanilla-js-redux/src/index.js
@@ -0,0 +1,51 @@
+import { createStore } from "redux";
+
+const lightDiv = document.getElementsByClassName("light")[0];
+const switchButton = document.getElementById("switch-btn");
+
+const counterHeadings = document.getElementsByTagName("h1")[0];
+const plusButton = document.getElementById("plus-btn");
+const minusButton = document.getElementById("minus-btn");
+
+// **** 액션 타입 정의
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+// **** 액션 생성함수 정의
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increment = diff => ({ type: INCREMENT, diff });
+const decrement = () => ({ type: DECREMENT });
+
+// **** 초깃값 설정
+const initialState = {
+  light: false,
+  counter: 0
+};
+
+// **** 리듀서 함수 정의
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 기존의 값은 그대로 두면서
+        light: !state.light // light 값 반전시키기
+      };
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + action.diff
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default:
+      // 지원하지 않는 액션의 경우 상태 유지
+      return state;
+  }
+}
+
+// **** 스토어 만들기
+const store = createStore(reducer);
