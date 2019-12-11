4097f6ed7d5cf05e304695724aaf303c91c403df
james.kim <james.kim@yht.co.kr>
2019-12-11 17:18:44 +0900

    mobx

--- mobx-with-react/src/index.js
+++ mobx-with-react/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react';
+import RootStore from './stores';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = new RootStore(); // *** 루트 스토어 생성
+
+ReactDOM.render(
+  <Provider {...root}> {/* *** ...root 으로 스토어 모두 자동으로 설정 */}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
